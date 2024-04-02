65395a06d239762e254ee93ab0eb273689f7a5d2
Aditya Pinnika <106610442+adityapinnika2003@users.noreply.github.com>
2024-04-02 17:19:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,127 +70,128 @@ def generate_summary(teacher_feedback):
 
 st.header('RAMACHANDRA COLLEGE OF ENGINEERING')
 st.title('STUDENT FEEDBACK ANALYZER')
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-
-csv=st.file_uploader('Upload Feedback File here')
-if csv:
-    df = pd.read_csv(csv)
-# Load the dataset
-# Specify the range of teachers to consider
-    def preprocess_text(text):
-        text = text.lower()
-
-    # Remove URLs, hashtags, mentions, and special characters
-        text = re.sub(r"http\S+|www\S+|@\w+|#\w+", "", text)
-        text = re.sub(r"[^\w\s]", "", text)
-
-    # Remove numbers/digits
-        text = re.sub(r'\b[0-9]+\b\s*', '', text)
-
-    # Remove punctuation
-        text = ''.join([char for char in text if char not in string.punctuation])
-
-    # Tokenize the text
-        tokens = word_tokenize(text)
-
-    # Remove stop words
-        stop_words = set(stopwords.words('english'))
-        tokens = [token for token in tokens if token not in stop_words]
-
-    # Lemmatize the words
-        lemmatizer = WordNetLemmatizer()
-        tokens = [lemmatizer.lemmatize(token) for token in tokens]
-
-    # Join tokens back into a single string
-        return ' '.join(tokens)
-
-    df['Processed_Feedback 1'] = df["Teacher 1"].apply(preprocess_text)
-    df['Processed_Feedback 2'] = df["Teacher 2"].apply(preprocess_text)
-    df['Processed_Feedback 3'] = df["Teacher 3"].apply(preprocess_text)
-    df['Processed_Feedback 4'] = df["Teacher 4"].apply(preprocess_text)
-    df['Processed_Feedback 5'] = df["Teacher 5"].apply(preprocess_text)
-    df['Sentiment_Scores 1'] = df['Processed_Feedback 1'].apply(lambda x: TextBlob(x).sentiment.polarity)
-    df['Sentiment_Scores 2'] = df['Processed_Feedback 2'].apply(lambda x: TextBlob(x).sentiment.polarity)
-    df['Sentiment_Scores 3'] = df['Processed_Feedback 3'].apply(lambda x: TextBlob(x).sentiment.polarity)
-    df['Sentiment_Scores 4'] = df['Processed_Feedback 4'].apply(lambda x: TextBlob(x).sentiment.polarity)
-    df['Sentiment_Scores 5'] = df['Processed_Feedback 5'].apply(lambda x: TextBlob(x).sentiment.polarity)
-
-    def sentiment_analyzer(score):
-        if score > 0.049889:
-            return 'positive'
-
-        elif score < 0.049889:
-            return 'negative'
+if st.button('Analyze'):
+    model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+    
+    csv=st.file_uploader('Upload Feedback File here')
+    if csv:
+        df = pd.read_csv(csv)
+    # Load the dataset
+    # Specify the range of teachers to consider
+        def preprocess_text(text):
+            text = text.lower()
+    
+        # Remove URLs, hashtags, mentions, and special characters
+            text = re.sub(r"http\S+|www\S+|@\w+|#\w+", "", text)
+            text = re.sub(r"[^\w\s]", "", text)
+    
+        # Remove numbers/digits
+            text = re.sub(r'\b[0-9]+\b\s*', '', text)
+    
+        # Remove punctuation
+            text = ''.join([char for char in text if char not in string.punctuation])
+    
+        # Tokenize the text
+            tokens = word_tokenize(text)
+    
+        # Remove stop words
+            stop_words = set(stopwords.words('english'))
+            tokens = [token for token in tokens if token not in stop_words]
+    
+        # Lemmatize the words
+            lemmatizer = WordNetLemmatizer()
+            tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    
+        # Join tokens back into a single string
+            return ' '.join(tokens)
+    
+        df['Processed_Feedback 1'] = df["Teacher 1"].apply(preprocess_text)
+        df['Processed_Feedback 2'] = df["Teacher 2"].apply(preprocess_text)
+        df['Processed_Feedback 3'] = df["Teacher 3"].apply(preprocess_text)
+        df['Processed_Feedback 4'] = df["Teacher 4"].apply(preprocess_text)
+        df['Processed_Feedback 5'] = df["Teacher 5"].apply(preprocess_text)
+        df['Sentiment_Scores 1'] = df['Processed_Feedback 1'].apply(lambda x: TextBlob(x).sentiment.polarity)
+        df['Sentiment_Scores 2'] = df['Processed_Feedback 2'].apply(lambda x: TextBlob(x).sentiment.polarity)
+        df['Sentiment_Scores 3'] = df['Processed_Feedback 3'].apply(lambda x: TextBlob(x).sentiment.polarity)
+        df['Sentiment_Scores 4'] = df['Processed_Feedback 4'].apply(lambda x: TextBlob(x).sentiment.polarity)
+        df['Sentiment_Scores 5'] = df['Processed_Feedback 5'].apply(lambda x: TextBlob(x).sentiment.polarity)
+    
+        def sentiment_analyzer(score):
+            if score > 0.049889:
+                return 'positive'
+    
+            elif score < 0.049889:
+                return 'negative'
+            else:
+                return 'neutral'
+        df['Sentiments 1'] = df['Sentiment_Scores 1'].apply(sentiment_analyzer)
+        df['Sentiments 2'] = df['Sentiment_Scores 2'].apply(sentiment_analyzer)
+        df['Sentiments 3'] = df['Sentiment_Scores 3'].apply(sentiment_analyzer)
+        df['Sentiments 4'] = df['Sentiment_Scores 4'].apply(sentiment_analyzer)
+        df['Sentiments 5'] = df['Sentiment_Scores 5'].apply(sentiment_analyzer)
+        total_reviews = len(df)
+    
+    # Count the number of negative sentiment labels
+        negative_count = df['Sentiments 1'].value_counts().get('negative', 0)
+        positive_count=  df['Sentiments 1'].value_counts().get('positive', 0)
+    
+    # Check if more than half of the reviews are negative
+        if positive_count > total_reviews / 2:
+            st.text("Majority Students feedback on Teacher 1  is Positive.")
+        elif positive_count==negative_count:
+            st.text("Majority Students feedback on Teacher 1  is Neutral.")
         else:
-            return 'neutral'
-    df['Sentiments 1'] = df['Sentiment_Scores 1'].apply(sentiment_analyzer)
-    df['Sentiments 2'] = df['Sentiment_Scores 2'].apply(sentiment_analyzer)
-    df['Sentiments 3'] = df['Sentiment_Scores 3'].apply(sentiment_analyzer)
-    df['Sentiments 4'] = df['Sentiment_Scores 4'].apply(sentiment_analyzer)
-    df['Sentiments 5'] = df['Sentiment_Scores 5'].apply(sentiment_analyzer)
-    total_reviews = len(df)
-
-# Count the number of negative sentiment labels
-    negative_count = df['Sentiments 1'].value_counts().get('negative', 0)
-    positive_count=  df['Sentiments 1'].value_counts().get('positive', 0)
-
-# Check if more than half of the reviews are negative
-    if positive_count > total_reviews / 2:
-        st.text("Majority Students feedback on Teacher 1  is Positive.")
-    elif positive_count==negative_count:
-        st.text("Majority Students feedback on Teacher 1  is Neutral.")
-    else:
-        st.text("Majority Students feedback on Teacher 1  is Negative.")
-    negative_count = df['Sentiments 2'].value_counts().get('negative', 0)
-    positive_count=  df['Sentiments 2'].value_counts().get('positive', 0)
-
-# Check if more than half of the reviews are negative
-    if positive_count > total_reviews / 2:
-        st.text("Majority Students feedback on Teacher 2  is Positive.")
-    elif positive_count==negative_count:
-        st.text("Majority Students feedback on Teacher 2  is Neutral.")
-    else:
-      st.text("Majority Students feedback on Teacher 2  is Negative.")
-    negative_count = df['Sentiments 3'].value_counts().get('negative', 0)
-    positive_count=  df['Sentiments 3'].value_counts().get('positive', 0)
-
-# Check if more than half of the reviews are negative
-    if positive_count > total_reviews / 2:
-        st.text("Majority Students feedback on Teacher 3  is Positive.")
-    elif positive_count==negative_count:
-        st.text("Majority Students feedback on Teacher 3  is Neutral.")
-    else:
-      st.text("Majority Students feedback on Teacher 3  is Negative.")
-    negative_count = df['Sentiments 4'].value_counts().get('negative', 0)
-    positive_count=  df['Sentiments 4'].value_counts().get('positive', 0)
-
-# Check if more than half of the reviews are negative
-    if positive_count > total_reviews / 2:
-        st.text("Majority Students feedback on Teacher 4  is Positive.")
-    elif positive_count==negative_count:
-        st.text("Majority Students feedback on Teacher 4  is Neutral.")
-    else:
-      st.text("Majority Students feedback on Teacher 4  is Negative.")
-    negative_count = df['Sentiments 5'].value_counts().get('negative', 0)
-    positive_count=  df['Sentiments 5'].value_counts().get('positive', 0)
-
-# Check if more than half of the reviews are negative
-    if positive_count > total_reviews / 2:
-        st.text("Majority Students feedback on Teacher 5  is Positive.")
-    elif positive_count==negative_count:
-        st.text("Majority Students feedback on Teacher 5  is Neutral.")
-    else:
-        st.text("Majority Students feedback on Teacher 5  is Negative.")
-        
-   
-    start_teacher = 1
-    end_teacher = 5  # Adjust as needed
-    # Generate summary for each teacher in the specified range
-    for i in range(start_teacher, end_teacher + 1):
-        if 'Teacher '+str(i) in df.columns and not df['Teacher '+str(i)].isnull().all():
-            teacher_feedback = df['Teacher '+str(i)].dropna().str.cat(sep=' ')
-            st.text("Summary of feedback for :"+'Teacher '+str(i))
-            st.text(generate_summary(teacher_feedback))
-
+            st.text("Majority Students feedback on Teacher 1  is Negative.")
+        negative_count = df['Sentiments 2'].value_counts().get('negative', 0)
+        positive_count=  df['Sentiments 2'].value_counts().get('positive', 0)
+    
+    # Check if more than half of the reviews are negative
+        if positive_count > total_reviews / 2:
+            st.text("Majority Students feedback on Teacher 2  is Positive.")
+        elif positive_count==negative_count:
+            st.text("Majority Students feedback on Teacher 2  is Neutral.")
         else:
-            st.text("No feedback available for Teacher"+str(i))
+          st.text("Majority Students feedback on Teacher 2  is Negative.")
+        negative_count = df['Sentiments 3'].value_counts().get('negative', 0)
+        positive_count=  df['Sentiments 3'].value_counts().get('positive', 0)
+    
+    # Check if more than half of the reviews are negative
+        if positive_count > total_reviews / 2:
+            st.text("Majority Students feedback on Teacher 3  is Positive.")
+        elif positive_count==negative_count:
+            st.text("Majority Students feedback on Teacher 3  is Neutral.")
+        else:
+          st.text("Majority Students feedback on Teacher 3  is Negative.")
+        negative_count = df['Sentiments 4'].value_counts().get('negative', 0)
+        positive_count=  df['Sentiments 4'].value_counts().get('positive', 0)
+    
+    # Check if more than half of the reviews are negative
+        if positive_count > total_reviews / 2:
+            st.text("Majority Students feedback on Teacher 4  is Positive.")
+        elif positive_count==negative_count:
+            st.text("Majority Students feedback on Teacher 4  is Neutral.")
+        else:
+          st.text("Majority Students feedback on Teacher 4  is Negative.")
+        negative_count = df['Sentiments 5'].value_counts().get('negative', 0)
+        positive_count=  df['Sentiments 5'].value_counts().get('positive', 0)
+    
+    # Check if more than half of the reviews are negative
+        if positive_count > total_reviews / 2:
+            st.text("Majority Students feedback on Teacher 5  is Positive.")
+        elif positive_count==negative_count:
+            st.text("Majority Students feedback on Teacher 5  is Neutral.")
+        else:
+            st.text("Majority Students feedback on Teacher 5  is Negative.")
+            
+       
+        start_teacher = 1
+        end_teacher = 5  # Adjust as needed
+        # Generate summary for each teacher in the specified range
+        for i in range(start_teacher, end_teacher + 1):
+            if 'Teacher '+str(i) in df.columns and not df['Teacher '+str(i)].isnull().all():
+                teacher_feedback = df['Teacher '+str(i)].dropna().str.cat(sep=' ')
+                st.text("Summary of feedback for :"+'Teacher '+str(i))
+                st.text(generate_summary(teacher_feedback))
+    
+            else:
+                st.text("No feedback available for Teacher"+str(i))
