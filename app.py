239d43ca831a605834e43af852d8435e44d5bdb7
Aditya Pinnika <106610442+adityapinnika2003@users.noreply.github.com>
2024-04-02 14:18:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,124 @@ from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import nltk
 nltk.download('punkt')
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_theme(style='white')
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+import re
+import string
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.sentiment import SentimentIntensityAnalyzer
+from textblob import TextBlob
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+
+import xgboost as xgb
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, precision_score, recall_score, f1_score
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.model_selection import KFold
+
+import warnings
+warnings.filterwarnings('ignore')
+def preprocess_text(text):
+    # Convert to lowercase
+    text = text.lower()
+
+    # Remove URLs, hashtags, mentions, and special characters
+    text = re.sub(r"http\S+|www\S+|@\w+|#\w+", "", text)
+    text = re.sub(r"[^\w\s]", "", text)
+
+    # Remove numbers/digits
+    text = re.sub(r'\b[0-9]+\b\s*', '', text)
+
+    # Remove punctuation
+    text = ''.join([char for char in text if char not in string.punctuation])
+
+    # Tokenize the text
+    tokens = word_tokenize(text)
+
+    # Remove stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [token for token in tokens if token not in stop_words]
+
+    # Lemmatize the words
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+
+    # Join tokens back into a single string
+    return ' '.join(tokens)
+
+df['Processed_Feedback 1'] = df["Teacher 1"].apply(preprocess_text)
+df['Processed_Feedback 2'] = df["Teacher 2"].apply(preprocess_text)
+df['Processed_Feedback 3'] = df["Teacher 3"].apply(preprocess_text)
+df['Processed_Feedback 4'] = df["Teacher 4"].apply(preprocess_text)
+df['Processed_Feedback 5'] = df["Teacher 5"].apply(preprocess_text)
+df['Sentiment_Scores 1'] = df['Processed_Feedback 1'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+#print(df['Sentiment_Scores'].describe())
+ st.text(df['Sentiment_Scores 1'])
+
+sns.histplot(df['Sentiment_Scores 1'])
+plt.title('Distribution of sentiment scores of Teacher 1')
+plt.ylabel('Frequency')
+plt.xlabel('Sentiment scores 1')
+plt.show()
+df['Sentiment_Scores 2'] = df['Processed_Feedback 2'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+#print(df['Sentiment_Scores'].describe())
+ st.text(df['Sentiment_Scores 2'])
+
+sns.histplot(df['Sentiment_Scores 2'])
+plt.title('Distribution of sentiment scores of Teacher 2')
+plt.ylabel('Frequency')
+plt.xlabel('Sentiment scores 2')
+plt.show()
+df['Sentiment_Scores 3'] = df['Processed_Feedback 3'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+#print(df['Sentiment_Scores'].describe())
+ st.text(df['Sentiment_Scores 3'])
+
+sns.histplot(df['Sentiment_Scores 3'])
+plt.title('Distribution of sentiment scores of Teacher 3')
+plt.ylabel('Frequency')
+plt.xlabel('Sentiment scores 3')
+plt.show()
+df['Sentiment_Scores 4'] = df['Processed_Feedback 4'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+#print(df['Sentiment_Scores'].describe())
+ st.text(df['Sentiment_Scores 4'])
+
+sns.histplot(df['Sentiment_Scores 4'])
+plt.title('Distribution of sentiment scores of Teacher 4')
+plt.ylabel('Frequency')
+plt.xlabel('Sentiment scores 4')
+plt.show()
+df['Sentiment_Scores 5'] = df['Processed_Feedback 5'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+#print(df['Sentiment_Scores'].describe())
+ st.text(df['Sentiment_Scores 5'])
+
+sns.histplot(df['Sentiment_Scores 5'])
+plt.title('Distribution of sentiment scores of Teacher 5')
+plt.ylabel('Frequency')
+plt.xlabel('Sentiment scores 5')
+plt.show()
 
 def generate_summary(teacher_feedback):
     # Tokenize the feedback into sentences
